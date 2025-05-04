6d20b686f0e89ea9e4fb3c14dd8f76c02c992c72
MihajloCumic <mihajlo.cumic1999@gmail.com>
2025-05-05 00:34:27 +0200

    napisani testovi za Routes

---
+++ tests/unit/RoutesTest.php
@@ -0,0 +1,100 @@
+<?php
+declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+use Src\enums\HttpMethod;
+use Src\routes\Route;
+use Src\routes\Routes;
+
+class RoutesTest extends TestCase
+{
+    private Routes $routes;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->routes = new Routes();
+    }
+    public function testAddsRoute(): void
+    {
+        //given
+        $method = HttpMethod::GET;
+        $path = '/path';
+        $handlerNames = [
+            'handler1',
+            'handler2'
+        ];
+        $controllerName = 'controller';
+        $controllerMethod = 'method';
+
+        $expected = [
+            'GET' => [
+                '/path' => new Route($handlerNames, $controllerName, $controllerMethod)
+            ]
+        ];
+
+        //when
+        $this->routes->addRoute($method, $path, $handlerNames, $controllerName, $controllerMethod);
+
+        //then
+        $this->assertEquals($expected, $this->routes->getRoutes());
+    }
+
+    public function testGetsRoute(): void
+    {
+        $method = HttpMethod::GET;
+        $path = '/path';
+        $handlerNames = [
+            'handler1',
+            'handler2'
+        ];
+        $controllerName = 'controller';
+        $controllerMethod = 'method';
+
+        $this->routes->addRoute($method, $path, $handlerNames, $controllerName, $controllerMethod);
+
+        $expected = new Route($handlerNames, $controllerName, $controllerMethod);
+
+        $route = $this->routes->getRoute($method, $path);
+        $this->assertEquals($expected, $route);
+    }
+
+    public function testNonExistingPath(): void
+    {
+        $method = HttpMethod::GET;
+        $path = '/path';
+        $handlerNames = [
+            'handler1',
+            'handler2'
+        ];
+        $controllerName = 'controller';
+        $controllerMethod = 'method';
+
+        $this->routes->addRoute($method, $path, $handlerNames, $controllerName, $controllerMethod);
+
+
+        $nonExistingPath = '/nonExisting';
+        $route = $this->routes->getRoute($method, $nonExistingPath);
+
+        $this->assertNull($route);
+    }
+
+    public function testNonExistingMethod(): void
+    {
+        $method = HttpMethod::GET;
+        $path = '/path';
+        $handlerNames = [
+            'handler1',
+            'handler2'
+        ];
+        $controllerName = 'controller';
+        $controllerMethod = 'method';
+
+        $this->routes->addRoute($method, $path, $handlerNames, $controllerName, $controllerMethod);
+
+
+        $nonExistingMethod = HttpMethod::POST;
+        $route = $this->routes->getRoute($nonExistingMethod, $path);
+
+        $this->assertNull($route);
+    }
+}
